e59539654ea6df82b6567ab3efe457f9e4d76a5a
niyi <akandefortunatus2021@gmail.com>
2026-03-02 11:34:40 +0100

    Add middleware

--- src/routes/habitRoutes.ts
+++ src/routes/habitRoutes.ts
@@ -1,4 +1,14 @@
 import { Router } from 'express'
+import { validateParams, validateBody } from '../middleware/validation.ts'
+import z from 'zod'
+
+const createHabitSchema = z.object({
+  name: z.string(),
+})
+
+const completeParamsSchema = z.object({
+  name: z.string(),
+})
 
 const router = Router()
 
@@ -10,7 +20,7 @@ router.get('/:id', (req, res) => {
   res.status(200).json({ message: 'Got one habit' })
 })
 
-router.post('/', (req, res) => {
+router.post('/', validateBody(createHabitSchema), (req, res) => {
   res.status(201).json({ message: 'Habit created' })
 })
 
@@ -18,8 +28,13 @@ router.delete('/:id', (req, res) => {
   res.json({ message: 'Habit deleted' })
 })
 
-router.post('/:id/complete', (req, res) => {
-  res.json({ message: 'Habit completed' })
-})
+router.post(
+  '/:id/complete',
+  validateParams(completeParamsSchema),
+  validateBody(createHabitSchema),
+  (req, res) => {
+    res.json({ message: 'Habit completed' })
+  },
+)
 
 export default router
--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,21 @@ import express from 'express'
 import authRoutes from './routes/authRoutes.ts'
 import userRoutes from './routes/userRoutes.ts'
 import habitRoutes from './routes/habitRoutes.ts'
+import cors from 'cors'
+import morgan from 'morgan'
+import helmet from 'helmet'
+import { isTest } from '../env.ts'
 
 const app = express()
+app.use(helmet())
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(
+  morgan('dev', {
+    skip: () => isTest(),
+  }),
+)
 
 app.use('/api/auth', authRoutes)
 app.use('/api/users', userRoutes)
