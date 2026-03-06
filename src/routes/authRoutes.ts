5f858446f84c498317fac898ccb5b682f7cc5840
niyi <akandefortunatus2021@gmail.com>
2026-03-07 00:33:09 +0100

    feat: implement authentication

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,13 +1,18 @@
 import { Router } from 'express'
+import { login, register } from '../controllers/authController.ts'
+import { validateBody } from '../middleware/validation.ts'
+import { insertUserSchema } from '../db/schema.ts'
+import { z } from 'zod'
+
+const loginSchema = z.object({
+  email: z.email('Invalid email address'),
+  password: z.string().min(6),
+})
 
 const router = Router()
 
-router.post('/register', (req, res) => {
-  res.status(201).json({ message: 'user signed up' })
-})
+router.post('/register', validateBody(insertUserSchema), register)
 
-router.post('/login', (req, res) => {
-  res.status(200).json({ message: 'user logged in' })
-})
+router.post('/login', validateBody(loginSchema), login)
 
 export default router
